0c38cae51528f449803c4d8af6c0e67c7ce2a7aa
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2021-09-25 01:02:26 -0700

    Update sorting of words in a string.py

--- sorting of words in a string.py
+++ sorting of words in a string.py
@@ -1,5 +1,7 @@
+#Enter a string separated by hyphens to get a sorted string with hyphens as the output
+
 def sorter(val):
-    val=val.lower()
+    val=val.lower() #upper and lower case have different values
     val=val.split("-")
     val.sort()
     val="-".join(val)
@@ -8,3 +10,4 @@ def sorter(val):
 
 val=input("Enter the string")
 sorter(val)
+#function call
